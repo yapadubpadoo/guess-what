2ff9d95f88be34822dc76995e6f7065d2b695a5c
Hiruma <yapadubpadoo@gmail.com>
2017-10-27 22:13:29 +0700

    now use rabbit

--- facebook-fetcher/helper/facebook_helper.py
+++ facebook-fetcher/helper/facebook_helper.py
@@ -1,5 +1,6 @@
 import facebook
-import log_helper
+import arrow
+from . import log_helper
 
 fb_logger = log_helper.get_logger('facebook-helper')
 
@@ -10,13 +11,21 @@ https://graph.facebook.com/v2.10/204234332938286_1891024787592557/reactions?acce
 &after=TVRBd01ERTRPRE0yTmpjeE5EWXlPakUxTURrd056SXlNRFk2TWpVME1EazJNVFl4TXc9PQZDZD
 '''
 
-
+POSTS_LIMIT_PER_PAGE = 100
 REACTION_LIMIT_PER_PAGE = 5000
-LOWER_POST_DATE = arrow.get('2017-08-31 17:00:00', 'YYYY-MM-DD HH:mm:ss')
-UPPER_POST_DATE = arrow.get('2017-10-01 17:00:00', 'YYYY-MM-DD HH:mm:ss')
+FIELDS_OF_POST="name,id,created_time,updated_time,message,message_tags,reactions"
 
 def get_facebook_graph(access_token):
  return facebook.GraphAPI(access_token=access_token, version="2.7")
+
+def get_next_page_of_feed(graph, page_id, after):
+    response = graph.get_object(
+        id=page_id + "/feed",
+        fields=FIELDS_OF_POST,
+        limit=POSTS_LIMIT_PER_PAGE,
+        after=after,
+    )
+    return response
 
 def get_reactions(graph, post_id, after):
     response = graph.get_object(
@@ -25,8 +34,6 @@ def get_reactions(graph, post_id, after):
         limit=REACTION_LIMIT_PER_PAGE
     )
     return response
-
-
 
 def get_all_reactions(graph, post_id, after):
     all_reactions = []
