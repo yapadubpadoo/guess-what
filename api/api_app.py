d24796f15e68f6d15873fdc7facb3c048147216b
Kittiporn Korprasertthaworn <izporn@gmail.com>
2017-10-29 05:05:45 +0700

    FIx bug datetime

--- api/api_app.py
+++ api/api_app.py
@@ -95,6 +95,7 @@ def handle_data(data):
             try:
                 inserted_id = tickets_collection.insert_one(ticket).inserted_id 
                 pprint.pprint("[++] Ticket ID = {} has been created".format(inserted_id))
+                ticket['created_time'] = arrow.get(ticket['created_time']).to('Asia/Bangkok').format('YYYY-MM-DD hh:ii:ss')
                 redis_client.publish('new-case', json.dumps({'data': ticket}))
             except DuplicateKeyError:
                 pprint.pprint("[--] Ignore duplicated ticket ID = {}".format(ticket['_id']))
