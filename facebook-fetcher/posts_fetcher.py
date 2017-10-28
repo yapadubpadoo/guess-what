afc1beae5ecb5ce801291a3111a0f455d888a8ed
Hiruma <yapadubpadoo@gmail.com>
2017-10-28 13:01:27 +0700

    add supervisor conf

--- facebook-fetcher/posts_fetcher.py
+++ facebook-fetcher/posts_fetcher.py
@@ -35,6 +35,7 @@ queue_channel.exchange_declare(
     exchange_type='fanout'
 )
 # create queues
+queue_channel.queue_declare(queue='fb:page::get-posts', durable=True)
 queue_channel.queue_declare(queue='fb:post::get-reactions', durable=True)
 queue_channel.queue_declare(queue='fb:post:get-comments', durable=True)
 # bind queues to an exchange
@@ -48,74 +49,83 @@ queue_channel.queue_bind(
 )
 
 graph = fb.get_facebook_graph(access_token=config['facebook']['user_token'])
-page_id = "204234332938286"
 
-feed = graph.get_object(
-    id=page_id + "/feed",
-    fields=fb.FIELDS_OF_POST,
-    limit=fb.POSTS_LIMIT_PER_PAGE
-)
+def callback(ch, method, properties, body):
+    page = json.loads(body.decode('utf-8'))
+    page_id = page["page_id"]
 
-page_count = 1
-fetch_next_page = True
+    feed = graph.get_object(
+        id=page_id + "/feed",
+        fields=fb.FIELDS_OF_POST,
+        limit=fb.POSTS_LIMIT_PER_PAGE
+    )
 
-while fetch_next_page:
-    logger.info('----------------- Page {} of page_id {}, posts length = {} -----------------'.format(
-            page_count,
-            page_id,
-            len(feed['data'])
-        ))
-    for post in feed['data']:
-        all_reactions = []
-        post_id = post['id']
-        post_created_time = arrow.get(post['created_time'])
-        logger.info('----------------- {} -----------------'.format(post_id))
-        if post_created_time < LOWER_POST_DATE or post_created_time > UPPER_POST_DATE: 
-            logger.info('[-] Drop outbound post, post_id = {}, created_time = {}'.format(
-                post_id, 
-                post_created_time
+    page_count = 1
+    fetch_next_page = True
+
+    while fetch_next_page:
+        logger.info('----------------- Page {} of page_id {}, posts length = {} -----------------'.format(
+                page_count,
+                page_id,
+                len(feed['data'])
             ))
-        else:
-            # insert post to db
-            logger.info('[+] Insert post to DB, post_id = {}, created_time = {}'.format(
-                post_id, 
-                post_created_time
-            ))
-            post['_id'] = post['id']
-            try:
-                post_id = posts_collection.insert_one(post).inserted_id 
-            except DuplicateKeyError:
-                pass
+        for post in feed['data']:
+            all_reactions = []
+            post_id = post['id']
+            post_created_time = arrow.get(post['created_time'])
+            logger.info('----------------- {} -----------------'.format(post_id))
+            if post_created_time < LOWER_POST_DATE or post_created_time > UPPER_POST_DATE: 
+                logger.info('[-] Drop outbound post, post_id = {}, created_time = {}'.format(
+                    post_id, 
+                    post_created_time
+                ))
+            else:
+                # insert post to db
+                logger.info('[+] Insert post to DB, post_id = {}, created_time = {}'.format(
+                    post_id, 
+                    post_created_time
+                ))
+                post['_id'] = post['id']
+                try:
+                    post_id = posts_collection.insert_one(post).inserted_id 
+                except DuplicateKeyError:
+                    pass
 
-            logger.info('[++] Send post to queue, post_id = {}, created_time = {}'.format(
-                post_id, 
-                post_created_time
-            ))
-            
-            # send to queue
-            queue_channel.basic_publish(
-                exchange=QUEUE_EXCHANGE,
-                routing_key='',
-                body=json.dumps(post),
-                properties=pika.BasicProperties(
-                    delivery_mode = 2, # make message persistent
+                logger.info('[++] Send post to queue, post_id = {}, created_time = {}'.format(
+                    post_id, 
+                    post_created_time
+                ))
+                
+                # send to queue
+                queue_channel.basic_publish(
+                    exchange=QUEUE_EXCHANGE,
+                    routing_key='',
+                    body=json.dumps(post),
+                    properties=pika.BasicProperties(
+                        delivery_mode = 2, # make message persistent
+                    )
                 )
+                pass
+        
+        if (arrow.get(feed['data'][-1]['created_time']) > LOWER_POST_DATE):
+            logger.info('Last created_time of this page is {}'.format(feed['data'][-1]['created_time']))
+            logger.info('Get next page ...')
+            feed = fb.get_next_page_of_feed(
+                graph=graph, 
+                page_id=page_id, 
+                after=feed['paging']['cursors']['after']
             )
-            pass
-    
-    if (arrow.get(feed['data'][-1]['created_time']) > LOWER_POST_DATE):
-        logger.info('Last created_time of this page is {}'.format(feed['data'][-1]['created_time']))
-        logger.info('Get next page ...')
-        feed = fb.get_next_page_of_feed(
-            graph=graph, 
-            page_id=page_id, 
-            after=feed['paging']['cursors']['after']
-        )
-        fetch_next_page = True
-    else:
-        logger.info('*** Last post of this page is older than {}'.format(LOWER_POST_DATE))
-        logger.info('*** OK, done')
-        fetch_next_page = False
+            fetch_next_page = True
+        else:
+            logger.info('*** Last post of this page is older than {}'.format(LOWER_POST_DATE))
+            logger.info('*** OK, done')
+            fetch_next_page = False
 
-    print("")
-    
+        print("")
+
+queue_channel.basic_qos(prefetch_count=1)
+queue_channel.basic_consume(
+    callback,
+    queue="fb:page::get-posts",
+    no_ack=False)
+queue_channel.start_consuming()
