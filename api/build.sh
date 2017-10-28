38682c0776d0c2d9fa730d97c82ca1b9bb54a6b6
Hiruma <yapadubpadoo@gmail.com>
2017-10-29 02:41:15 +0700

    always reload api after build

--- api/build.sh
+++ api/build.sh
@@ -2,4 +2,4 @@
 echo "Copy supervisor configuration for api service"
 sudo cp -f ./api.conf /etc/supervisor/conf.d/api.conf
 sudo supervisorctl reread
-sudo supervisorctl update
+sudo supervisorctl reload
