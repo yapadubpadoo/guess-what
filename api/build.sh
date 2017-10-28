bd3577f08958baf10afca76cd9ad974de2a037e8
Hiruma <yapadubpadoo@gmail.com>
2017-10-29 02:48:05 +0700

    restart api instead of supervisor

--- api/build.sh
+++ api/build.sh
@@ -2,4 +2,5 @@
 echo "Copy supervisor configuration for api service"
 sudo cp -f ./api.conf /etc/supervisor/conf.d/api.conf
 sudo supervisorctl reread
-sudo supervisorctl reload
+sudo supervisorctl restart guess-what-api-uwsgi:*
+sudo supervisorctl restart guess-what-api-direct:*
